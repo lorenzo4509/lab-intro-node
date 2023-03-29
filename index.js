87082758fb1f29473959c6be4e4d4a276b4432f5
Lorenzo Hernandez <lorenzorhdez@hotmail.com>
2023-03-29 21:25:40 +0200

    done

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let i = 0;
+    while (i < this.length && this.items[i] < item) {
+      i++;
+    }
+    this.items.splice(i, 0, item);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= 0 && pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
